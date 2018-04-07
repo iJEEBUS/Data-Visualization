9ecb1e1178f0cf5c9a4fa102c96b99dc88427e00
Ronald Rounsifer <ronrounsifer@me.com>
2018-04-06 21:14:01 -0400

    pandas / numpy added

---
+++ pandas-numpy.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+
+data = pd.DataFrame (
+    {"Major":["CS", "IS", "MBI", "STAT", "OTHER", "CS"],
+    "Happiness":[10,10,10,10,2,10]},
+    index=["Jon", "Jill", "Javhah", "Jessica", "Joe", "Jack"]
+    )
+
+print("\nCounts of each value in the pandas DataFrame: \n %s" % (data["Major"].value_counts()))
+print("\nNumber of data points in the pandas DataFrame:\n %s" % (len(data)))
+print("\nDescription of the pandas DataFrame:\n %s" % (data.describe()))
+print("\nPrinting of the pandas DataFrame:\n %s" % (data))
+print("\nAccessing the Major for the second record:\n %s" % (data["Major"][1]))
